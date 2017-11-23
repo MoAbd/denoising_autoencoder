d5bfb6002f57c246fa294705b93d67bfc4671a6a
Elliot Greenlee <egreenle@vols.utk.edu>
2017-11-22 19:58:15 -0500

    initial commit

---
+++ utils.py
@@ -0,0 +1,96 @@
+from scipy import misc
+import tensorflow as tf
+import numpy as np
+
+# ############# #
+#   Utilities   #
+# ############# #
+
+
+def xavier_init(fan_in, fan_out, const=1):
+    """ Xavier initialization of network weights.
+    https://stackoverflow.com/questions/33640581/how-to-do-xavier-initialization-on-tensorflow
+
+    :param fan_in: fan in of the network (n_features)
+    :param fan_out: fan out of the network (n_components)
+    :param const: multiplicative constant
+    """
+    low = -const * np.sqrt(6.0 / (fan_in + fan_out))
+    high = const * np.sqrt(6.0 / (fan_in + fan_out))
+    return tf.random_uniform((fan_in, fan_out), minval=low, maxval=high)
+    
+
+def gen_batches(data, batch_size):
+    """ Divide input data into batches.
+
+    :param data: input data
+    :param batch_size: size of each batch
+
+    :return: data divided into batches
+    """
+    data = np.array(data)
+
+    for i in range(0, data.shape[0], batch_size):
+        yield data[i:i+batch_size]
+
+
+def masking_noise(X, v):
+    """ Apply masking noise to data in X, in other words a fraction v of elements of X
+    (chosen at random) is forced to zero.
+
+    :param X: array_like, Input data
+    :param v: int, fraction of elements to distort
+
+    :return: transformed data
+    """
+    X_noise = X.copy()
+
+    n_samples = X.shape[0]
+    n_features = X.shape[1]
+
+    for i in range(n_samples):
+        mask = np.random.randint(0, n_features, v)
+
+        for m in mask:
+            X_noise[i][m] = 0.
+
+    return X_noise
+
+
+def salt_and_pepper_noise(X, v):
+    """ Apply salt and pepper noise to data in X, in other words a fraction v of elements of X
+    (chosen at random) is set to its maximum or minimum value according to a fair coin flip.
+    If minimum or maximum are not given, the min (max) value in X is taken.
+
+    :param X: array_like, Input data
+    :param v: int, fraction of elements to distort
+
+    :return: transformed data
+    """
+    X_noise = X.copy()
+    n_features = X.shape[1]
+
+    mn = X.min()
+    mx = X.max()
+
+    for i, sample in enumerate(X):
+        mask = np.random.randint(0, n_features, v)
+
+        for m in mask:
+
+            if np.random.random() < 0.5:
+                X_noise[i][m] = mn
+            else:
+                X_noise[i][m] = mx
+
+    return X_noise
+
+
+def gen_image(img, width, height, outfile, img_type='grey'):
+    assert len(img) == width * height or len(img) == width * height * 3
+
+    if img_type == 'grey':
+        misc.imsave(outfile, img.reshape(width, height))
+
+    elif img_type == 'color':
+        misc.imsave(outfile, img.reshape(3, width, height))
